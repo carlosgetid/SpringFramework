0cd011b8eeb53abed8dd698c286ffac5579d39bc
paulable <carlospgp13@gmail.com>
2020-07-12 18:05:17 -0500

    Learning Spring
    Hello World
    Hello World using Thymeleaf
    Sharing information using Thymeleaf
    	Expression Language
    Sharing values of an object of a class in the view

---
+++ Leccion02/HolaMundoThymeleaf/src/test/java/pe/com/gm/HolaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.gm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
